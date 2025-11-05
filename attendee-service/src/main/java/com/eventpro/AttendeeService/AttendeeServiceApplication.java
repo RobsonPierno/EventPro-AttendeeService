f632b11cc2c2c17fdca037bb0d2489477bfb4839
RobsonPierno <robson.pierno@gmail.com>
2025-11-05 11:49:24 +0100

    Removing @EnableFeignClients

--- attendee-service/src/main/java/com/eventpro/AttendeeService/AttendeeServiceApplication.java
+++ attendee-service/src/main/java/com/eventpro/AttendeeService/AttendeeServiceApplication.java
@@ -2,12 +2,10 @@ package com.eventpro.AttendeeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableFeignClients(basePackages = "com.eventpro.AttendeeService.client")
 public class AttendeeServiceApplication {
 
 	public static void main(String[] args) {
